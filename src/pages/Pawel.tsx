b717b2ebdea1a32571254e7575e63cc876c5da9f
PawelSzoltysek93 <pawel.szoltysek@dci-student.org>
2025-08-11 14:08:14 +0200

    test commit + Pawel.tsx created

---
+++ src/pages/Pawel.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Pawel = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-4xl font-bold">Working on my site...</p>
+      <img
+        src="https://media1.tenor.com/m/6yZRyjWnwGEAAAAC/warcraft-gaming.gif"
+        alt="peon"
+        style={{ width: "500px", borderRadius: "8px" }}
+      />
+    </div>
+  );
+};
+
+export default Pawel;
